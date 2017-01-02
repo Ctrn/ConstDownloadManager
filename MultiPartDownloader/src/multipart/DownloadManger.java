8eeeb01c58300d0a3c54d47a936d11c3232d3091
saad <saad@saad-hazem>
2017-01-02 21:20:48 +0200

    Add DownloadManger Class Change-Id: I04101985ec7248e58cc3f19b58b9bca97e120d4d Signed-off-by: saad Hazem <saadajaj35@gmail.com>

---
+++ MultiPartDownloader/src/multipart/DownloadManger.java
@@ -0,0 +1,37 @@
+package multipart;
+
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class DownloadManger {
+	
+	public DownloadManger()
+	{
+		
+		}
+	//Open the URL of the Designated and Return its Input Data -Bytes- 
+	public static byte[] downloadURL(InputStream openedURL)
+	 		throws IOException, EOFException {
+	 		int sizeOfRead;
+	 		try {
+	 			sizeOfRead = new DataInputStream(openedURL).readInt();
+	 		} catch(EOFException e) { // no more sub-files
+	 			return null;
+	 		}
+	 
+	 		byte[] dataInURL = new byte[sizeOfRead];
+	 		int readedCovrage = 0;
+	 		while(readedCovrage < sizeOfRead) {
+	 			int readLine = openedURL.read(dataInURL, readedCovrage, sizeOfRead-readedCovrage);
+	 			if(readLine==-1)
+	 				throw new EOFException("Stream Reached Line: "
+	 									+readedCovrage+" With a Bytes of "
+	 											+sizeOfRead);
+	 			readedCovrage += readLine;
+	 		}
+	 		return dataInURL;
+	 	}
+	 }
+
