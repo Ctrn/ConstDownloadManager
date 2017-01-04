16b03d29623370cf5fa42357fe306e8406b73fc6
saad <saad@saad-hazem>
2017-01-04 19:53:50 +0200

    Add the DownloadManager
    Change-Id: Ibbae4009ff6fbaaa8c44dfd4d15d45e484a57d70
    Change-Id: I000000000000000000000000000000000000000I
    Signed-off-by: saad <saadajaj35@gmail.com>

--- MultiPartDownloader/src/multipart/DownloadManger.java
+++ MultiPartDownloader/src/multipart/DownloadManger.java
@@ -4,7 +4,7 @@ import java.io.DataInputStream;
 import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
-
+ 
 public class DownloadManger { 
 	
 	public DownloadManger()
