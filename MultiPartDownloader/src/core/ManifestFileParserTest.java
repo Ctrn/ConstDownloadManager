fc8627c7d30be3a1298ec0565563dd24ac4d71ee
hanan <hanan@192.168.1.10>
2017-01-04 21:46:48 +0200

    File Parser and ParserTest
    Change-Id: Icb1b38b72e27d027905a45e01cdc1d676d59de0b
    Signed-off-by: hanan <hanan@192.168.1.10>

---
+++ MultiPartDownloader/src/core/ManifestFileParserTest.java
@@ -0,0 +1,32 @@
+package core;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class ManifestFileParserTest {
+	
+
+	@Test
+	public void testGetArreyListFromInputStreamFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGeMDtArreyListFromArrayList() {
+		fail("Not yet implemented");	
+	}
+
+	@Test
+	public void testPrintArraylist() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testParseArreylist() {
+		fail("Not yet implemented");
+	}
+
+}
