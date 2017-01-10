4475761157795179f71df1e80c9a591ceae56e56
saad <saad@saad-hazem>
2017-01-10 20:05:39 +0200

    Add Multipart Class to the multipart package
    Change-Id: I09e06b0388e9de6892560c8070d39ef93bffe9e6
    Signed-off-by: saad <saadajaj35@gmail.com>

---
+++ MultiPartDownloader/src/multipart/Multipart.java
@@ -0,0 +1,14 @@
+package multipart;
+
+import java.io.InputStream;
+
+public class Multipart {
+
+	public Multipart(){
+		
+	}
+	//Open the Stream by the Assigned url and return the Input Stream of the File
+	public  InputStream openStream(String url){
+		return null;
+	}
+}
