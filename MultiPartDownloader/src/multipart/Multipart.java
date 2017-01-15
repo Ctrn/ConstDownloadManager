51298092cb992ebf56016c5f1be7afea046dae84
saad <saad@saad-hazem>
2017-01-15 20:07:22 +0200

    Class Multipart, Implementation of  openStream() Method
    Change-Id: Ia5c0a6bec7c7789dbf7dde09c12f852abf55b094
    Signed-off-by: saad <saadajaj35@gmail.com@>

--- MultiPartDownloader/src/multipart/Multipart.java
+++ MultiPartDownloader/src/multipart/Multipart.java
@@ -1,6 +1,9 @@
 package multipart;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Multipart {
 
@@ -8,7 +11,9 @@ public class Multipart {
 		
 	}
 	//Open the Stream by the Assigned url and return the Input Stream of the File
-	public  InputStream openStream(String url){
-		return null;
+	public static InputStream openStream(String url) throws IOException,MalformedURLException {
+		InputStream input = null; //intalization of input stream 
+			input = new URL(url).openStream();//open the stream based on the url
+					return input;
 	}
 }
