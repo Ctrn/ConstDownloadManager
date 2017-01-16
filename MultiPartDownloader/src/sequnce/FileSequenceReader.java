65b2b6189a815599d3983e3edb13fa177eec3edd
hanan <hanan@192.168.1.10>
2017-01-17 01:30:49 +0200

    Create unit test for file sequence reader and multipat Change-Id: I4fe6ccb55de3a8042226b72c9f23c0a3eef313bd Signed-off-by: hanan <hanan@192.168.1.10>

--- MultiPartDownloader/src/sequnce/FileSequenceReader.java
+++ MultiPartDownloader/src/sequnce/FileSequenceReader.java
@@ -7,7 +7,9 @@ import java.io.InputStream;
 
 /**
  * Static class for reading from a file-sequence stream.
-
+ * <p>
+ * The format for file-sequence streams can be found
+ * 
  */
 public class FileSequenceReader {
 	/**
@@ -16,27 +18,27 @@ public class FileSequenceReader {
 	 * If no sub-files remain, returns null. If the stream ends prematurely,
 	 * throws an EOFException.
 	 */
-	public static byte[] readOneFile(InputStream sequence) 
+	public static byte[] readOneFile(InputStream sequence)
 		throws IOException, EOFException {
 		// sequence files consist of a (4-byte) int giving the size of the sub-file,
 		// followed by the sub-file, followed by another size, followed by the sub-file,
 		// and so on until EOF
-		int sizeOfStreamedSeq = 0;
-		 		// note that this hides errors involving less than 4 trailing bytes:
-		 		try {
-		 			sizeOfStreamedSeq = new DataInputStream(sequence).readInt();
-		 		} catch(EOFException e) { // no more sub-files
-		 			return null;
-		 		}
-		 
-		 		byte[] streamData = new byte[sizeOfStreamedSeq];
-		 		int readData = 0;
-		 		while(readData<sizeOfStreamedSeq) {
-		 			int readedStreamData = sequence.read(streamData, readData, sizeOfStreamedSeq-readData);
-		 			if(readedStreamData==-1)
-		 				throw new EOFException("Stream end @: "+readedStreamData+" Bytes: "+sizeOfStreamedSeq+" -Byte of file!");
-		 			readData += readedStreamData;
-		 		}
-		 		return streamData;
-		 	}		 
+		int size;
+		// note that this hides errors involving less than 4 trailing bytes:
+		try {
+			size = new DataInputStream(sequence).readInt();
+		} catch(EOFException e) { // no more sub-files
+			return null;
+		}
+
+		byte[] data = new byte[size];
+		int read = 0;
+		while(read<size) {
+			int justRead = sequence.read(data, read, size-read);
+			if(justRead==-1)
+				throw new EOFException("stream ended after only "+read+" bytes of "+size+"-byte sub-file!");
+			read += justRead;
+		}
+		return data;
 	}
+}
