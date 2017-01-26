8fa06daa747b5cd4df94c60611254ba5113f1d5f
saad <saad@saad-hazem>
2017-01-26 20:01:25 +0200

    Adding The Image Node - Class
    Change-Id: Iba53339471adddf01bd1725efb0f8bbd1df03e3d
    Signed-off-by: saad <saadajaj35@gmail.com>

---
+++ MultiPartDownloader/src/core/ImageNode.java
@@ -0,0 +1,20 @@
+package core;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+public class ImageNode implements Segment{
+
+	@Override
+	public String readContntFromURL() throws IOException, MalformedURLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean checkFileType(String url) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
