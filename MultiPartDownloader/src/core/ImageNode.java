9cbf49b0988a280661cf289d99b1fbcc1a7e061e
saad <saad@saad-hazem>
2017-01-26 22:41:13 +0200

    Modifed ImageNode class
    Change-Id: I90f3838f8a2a89896f21ade0cb0b232705b2e7f6
    Signed-off-by: saad <saadajaj35@gmail.com>

--- MultiPartDownloader/src/core/ImageNode.java
+++ MultiPartDownloader/src/core/ImageNode.java
@@ -1,28 +1,53 @@
 package core;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.apache.log4j.Logger;
+
 public class ImageNode implements Segment{
-
-
+	
+	private String imageURL = ""; 
+	private String imageContent = ""; 
+	
+	private final static String[] fileTypeToRead = {"jpeg","png","gif"};
+	private final static String sequnceType = "-seq";
+	
+	public ImageNode(String imageURL){
+		this.imageURL = imageURL;
+	}
+	
+	final Logger logger = Logger.getLogger(ImageNode.class);
+	
+	public String readContntFromURL() throws  IOException,MalformedURLException{
+		return "";
+	}
+	
 	@Override
 	public URL openURL() throws IOException, MalformedURLException {
-		// TODO Auto-generated method stub
-		return null;
+		URL  segmentPath = new URL(imageURL);
+			logger.trace("URL of: "+imageURL+" IS Successfuly Opened");
+				return segmentPath;
 	}
-
 	@Override
 	public boolean isFileToRead() {
-		// TODO Auto-generated method stub
-		return false;
+		boolean checkRes = false;
+			String fileType = imageURL.substring(imageURL.lastIndexOf(".")+1);
+				for(int i = 0 ; i < 3 ; i++ ){
+					if(fileTypeToRead[i].equalsIgnoreCase(fileType)){
+						logger.trace("The Image is With the Extnetion of: "+fileType);
+							checkRes = true;
+					}
+				}
+		return checkRes;
 	}
-
 	@Override
 	public boolean isASequnce() {
-		// TODO Auto-generated method stub
-		return false;
+		String fileType = imageURL.substring(imageURL.lastIndexOf("-"));
+		logger.trace("Nested Sequnce..");
+			return sequnceType.equalsIgnoreCase(fileType);
 	}
-
 }
