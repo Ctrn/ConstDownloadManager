d1aa78a5b5fb048af9d2540c61855d6815be1fb8
saad <saad@saad-hazem>
2017-01-26 20:39:20 +0200

    Segment Reading Implemntation - Modified ImageNode,Segment,TxtNode
    Change-Id: Ie4691edb185602f9fe9acdc0ff33d82b974f9ae5
    Signed-off-by: saad <saad@saad-hazem>

--- MultiPartDownloader/src/core/ImageNode.java
+++ MultiPartDownloader/src/core/ImageNode.java
@@ -2,17 +2,25 @@ package core;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URL;
 
 public class ImageNode implements Segment{
 
+
 	@Override
-	public String readContntFromURL() throws IOException, MalformedURLException {
+	public URL openURL() throws IOException, MalformedURLException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public boolean checkFileType(String url) {
+	public boolean isFileToRead() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isASequnce() {
 		// TODO Auto-generated method stub
 		return false;
 	}
