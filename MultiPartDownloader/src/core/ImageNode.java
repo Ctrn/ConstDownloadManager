e11a7afd4fae4596ffbdda5898684f3fc6d13363
hanan <hanan@192.168.1.6>
2017-01-27 06:47:46 +0200

    Change-Id: Ic8ba3bbeddef52db4af211c97bf108758849b3c8 Signed-off-by: hanan <hanan@192.168.1.6>

--- MultiPartDownloader/src/core/ImageNode.java
+++ MultiPartDownloader/src/core/ImageNode.java
@@ -1,28 +1,68 @@
 package core;
 
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
+import org.apache.log4j.Logger;
+
 public class ImageNode implements Segment{
+	
+	private String imageURL = ""; 
+	private BufferedImage imageContent = null; 
+	
+	private final static String[] fileTypeToRead = {"jpeg","png","gif"};
+	private final static String sequnceType = "-seq";
+	
+	public ImageNode(String imageURL){
+		this.imageURL = imageURL;
+	}
 
-
+	final Logger logger = Logger.getLogger(ImageNode.class);
+	
+	public ImageIcon readContntFromURL() throws  IOException,MalformedURLException{
+		// Get Image Path
+		URL imagePath = openURL();
+				// Get BufferedImage
+		      		if(isFileToRead()){
+		      			imageContent = ImageIO.read(imagePath); 
+		      			logger.trace("Image Segment Of  URL: "+imageURL+" is Buffered");
+		      		}
+		      		else if(isASequnce()){
+		      			//TO DO 
+		      		}
+		      		return new ImageIcon(imageContent);
+	}
+	
 	@Override
 	public URL openURL() throws IOException, MalformedURLException {
-		// TODO Auto-generated method stub
-		return null;
+		URL  segmentPath = new URL(imageURL);
+			logger.trace("URL of: "+imageURL+" IS Successfuly Opened");
+				return segmentPath;
 	}
-
 	@Override
 	public boolean isFileToRead() {
-		// TODO Auto-generated method stub
-		return false;
+		boolean checkRes = false;
+			String fileType = imageURL.substring(imageURL.lastIndexOf(".")+1);
+				for(int i = 0 ; i < 3 ; i++ ){
+					if(fileTypeToRead[i].equalsIgnoreCase(fileType)){
+						logger.trace("The Image is With the Extnetion of: "+fileType);
+							checkRes = true;
+					}
+				}
+		return checkRes;
 	}
-
 	@Override
 	public boolean isASequnce() {
-		// TODO Auto-generated method stub
-		return false;
+		String fileType = imageURL.substring(imageURL.lastIndexOf("-"));
+		logger.trace("Nested Sequnce..");
+			return sequnceType.equalsIgnoreCase(fileType);
 	}
-
 }
